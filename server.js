4d57fb1b262c2b0c3d026adfd701986547746db3
kdublam <kdublam@gmail.com>
2021-01-03 17:36:04 -0800

    Add instructions

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var PORT = 5000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname+'/src'));
+
 
 
 
