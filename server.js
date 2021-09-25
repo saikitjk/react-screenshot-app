2e255e50754bf36e3f9028805c784e4fa34fac67
JK <saikitjk@gmail.com>
2021-09-25 16:00:30 -0700

    added dynamic port, cors, puppeteer-cluster

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
-var puppeteer = require("puppeteer");
-var fs = require("fs");
+const express = require("express");
+const path = require("path");
+const puppeteer = require("puppeteer");
+const { Cluster } = require("puppeteer-cluster");
+const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 5000;
+var PORT = process.env.PORT || 5000;
+var cors = require("cors");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
