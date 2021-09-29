4d6a6c1df081ad744cf38749a2de5f4e916f053d
JK <saikitjk@gmail.com>
2021-09-28 23:24:14 -0700

    added app name and intro as header content

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -3,5 +3,13 @@ import { Jumbotron } from "reactstrap";
 import "./header.css";
 
 export default function header() {
-  return <div></div>;
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Screenshot App</h1>
+        <h3>Capture screenshot of any URL</h3>
+        <hr></hr>
+      </Jumbotron>
+    </div>
+  );
 }
