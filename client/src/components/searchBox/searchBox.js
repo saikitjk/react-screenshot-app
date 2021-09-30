6fdcb13c7a2b2f1629929638c486b816ff5a731d
JK <saikitjk@gmail.com>
2021-09-29 23:34:12 -0700

    added 4 buttons and their properties in scarchBox

--- client/src/components/searchBox/searchBox.js
+++ client/src/components/searchBox/searchBox.js
@@ -14,6 +14,26 @@ export default function searchBox() {
           placeholder="ex. https://www.google.com/"
         />
       </FormGroup>
+
+      <div className="text-right">
+        <Button color="primary" className="resetBtn" type="submit" id="reset">
+          Reset
+        </Button>
+        <Button
+          color="primary"
+          className="savetBtn"
+          type="submit"
+          id="save-btn"
+        >
+          Grab screenshots
+        </Button>
+        <Button color="primary" className="loadBtn" type="button" id="load-btn">
+          Processing. Please wait...
+        </Button>
+        <Button color="primary" className="dlBtn" type="submit" id="download">
+          Download File
+        </Button>
+      </div>
     </div>
   );
 }
