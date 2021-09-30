334d05191aa7a8ec702ec944e64db0c55e549c66
JK <saikitjk@gmail.com>
2021-09-29 23:07:34 -0700

    imported searchbox and Main class component into main.js

---
+++ client/src/components/main/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBox from "./searchBox/searchBox";
+import { Card, CardTitle, CardBody } from "reactstrap";
+import "./main.css";
+
+class Main extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export { Main as default };
