ba9cd571f2c24bd3da0b19832f11c5b1deb4dab1
JK <saikitjk@gmail.com>
2021-09-29 22:43:17 -0700

    imported header, main, instruction  and layout components into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/header";
+import Main from "./components/main/main";
+import Instruction from "./components/instruction/instruction";
+import { Row, Container, Col } from "reactstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Header />
+        <Row>
+          <Col>
+            <Main />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Instruction />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
