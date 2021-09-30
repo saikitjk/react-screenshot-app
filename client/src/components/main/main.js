78e4d39799ff05588040602759b6b03d67d1a199
JK <saikitjk@gmail.com>
2021-09-30 15:45:20 -0700

    added react props to pass data from searchbox input

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -4,13 +4,18 @@ import { Card, CardTitle, CardBody } from "reactstrap";
 import "./main.css";
 
 class Main extends React.Component {
+  state = { url: "", urlArray: [] };
   render() {
     return (
       <div>
         <Card>
           <CardTitle>
             <CardBody>
-              <SearchBox />
+              <SearchBox
+                //  inputURL={this.state.url}
+                handleInputChange={this.handleInputChange}
+                handleGrab={this.handleGrab}
+              />
             </CardBody>
           </CardTitle>
         </Card>
