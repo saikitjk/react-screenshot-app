939a7f7c9d17ff85fec0c2f12427be00a2347c52
JK <saikitjk@gmail.com>
2021-09-30 17:31:33 -0700

    added preventDefault and logic to generate random sessid in handleGrab function

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -9,11 +9,25 @@ class Main extends React.Component {
     urlArray: [],
   };
 
+  //input box in searchBox
   handleInputChange = (event) => {
     const url = event.target.value;
     //onsole.log("url " + url) //this console.log can see the onChange of user's input
     this.setState({ urlArray: url.trim().match(/[^\r\n]+/g) });
     //console.log("urlArray: " + this.state.urlArray)
+  };
+
+  //submit button
+  handleGrab = (event) => {
+    event.preventDefault();
+    //generate random sessID
+    let ranGen = () => {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    };
+
+    var sessID = ranGen();
   };
 
   render() {
