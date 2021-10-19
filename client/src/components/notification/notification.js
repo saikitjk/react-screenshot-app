43dddfa945beabeed061a38527984ed245749df5
JK <saikitjk@gmail.com>
2021-10-18 22:27:27 -0700

    added two reactstrap alerts into a conditional rendering

--- client/src/components/notification/notification.js
+++ client/src/components/notification/notification.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Alert } from "reactstrap";
+import "./notification.css";
 
-export default function notification() {
-  return <div></div>;
+export default function notification(props) {
+  return (
+    <div>
+      <div>
+        {props.displayInfoMsg ? (
+          <Alert color="info">{props.message}</Alert>
+        ) : null}
+      </div>
+      <div>
+        {props.displayErrorMsg ? (
+          <Alert color="danger">{props.message}</Alert>
+        ) : null}
+      </div>
+    </div>
+  );
 }
