5d07292b790419f5af8db473f666d08bcc90e694
JK <saikitjk@gmail.com>
2021-11-28 14:45:47 -0800

    added flexbox-container, flexbox items and css  for instruction page

---
+++ client/src/components/instruction/instruction.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./instruction.css";
+
+export default function instruction() {
+  return (
+    <div className="instructionFlexbox-container">
+      <div className="flexbox-item-left">left</div>
+      <div className="flexbox-item-right">right</div>
+    </div>
+  );
+}
