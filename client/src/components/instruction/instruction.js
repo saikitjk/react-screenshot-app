4d036ada4e84c1e5ec0d40f4a3b63fa9284de9af
JK <saikitjk@gmail.com>
2021-12-26 00:33:17 -0800

    added class component in instruction.js

--- client/src/components/instruction/instruction.js
+++ client/src/components/instruction/instruction.js
@@ -4,16 +4,20 @@ import ActiveThumbnailWindow from "./thumbnailGallery/activeThumbnailWindow";
 import ThumbnailGrid from "./thumbnailGallery/thumbnailGrid";
 import TextArea from "./thumbnailGallery/textArea";
 
-export default function instruction() {
-  return (
-    <div className="instructionFlexbox-container">
-      <div className="flexbox-item-left">
-        <ActiveThumbnailWindow />
-        <ThumbnailGrid />
+class Instruction extends React.Component {
+  render() {
+    return (
+      <div className="instructionFlexbox-container">
+        <div className="flexbox-item-left">
+          <ActiveThumbnailWindow />
+          <ThumbnailGrid />
+        </div>
+        <div className="flexbox-item-right">
+          <TextArea />
+        </div>
       </div>
-      <div className="flexbox-item-right">
-        <TextArea />
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
+export { Instruction as default };
